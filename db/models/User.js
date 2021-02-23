85fcf98e34ad0c69838c9a5d059876ed0d0cb4fe
Abdullah ALmeshekah <meshekah@MacBook-Pro.local>
2021-02-23 12:28:33 +0300

    list&&signin&&signup

---
+++ db/models/User.js
@@ -0,0 +1,34 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define("User", {
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: {
+            args: true,
+            msg: "Username already exists"
+        },
+        allowNull: false,
+      },
+
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        isEmail: true,
+      },
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+
+    },{timestamps:false},);
+    return User;
+  };
